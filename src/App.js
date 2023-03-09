4a8c20f01b83b93998445078fdeae70b44f30ba9
suneha <suneha.obeja@gmail.com>
2023-03-09 17:42:06 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User.js';
 
 function App() {
   return (
     <div className="App">
     <h1>App component</h1>
+    <User/>
     </div>
   );
 }
